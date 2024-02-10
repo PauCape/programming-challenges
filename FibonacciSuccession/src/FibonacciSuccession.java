0fd362d0794aec0de29046ca617b459079a09ec7
Pau Capellá Ballester <pau.capella@hotmail.es>
2024-02-10 17:21:10 +0100

    fibonacci exercise solved

--- FibonacciSuccession/src/FibonacciSuccession.java
+++ FibonacciSuccession/src/FibonacciSuccession.java
@@ -1,14 +1,24 @@
 public class FibonacciSuccession {
     public static void main(String[] args) throws Exception {
-        System.out.println("Fibonacci succession Exercise:");
+        System.out.println("Fibonacci succession Exercise: \n");
 
-        int auxInitial = 0;
-        int auxSecond = 0;
+        // Initializad like the first iteration
+        long auxInitial = 1;
+        long auxSecond = 1;
+        long auxResult = 1;
 
+        // This is not required but like this the result is better
         System.out.print(0);
 
         for (int i = 1; i <= 50; i++) {
-            System.out.print("," + i);
+
+                System.out.print(", " + auxInitial);
+
+                auxResult = auxInitial + auxSecond;
+    
+                auxInitial = auxSecond;
+                
+                auxSecond = auxResult;
         }
     }
 }
